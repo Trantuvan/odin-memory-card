b819f9ac4ef9573e639b438d960f222ea5452b78
trantuvan <trantuvan.kan@gmail.com>
2023-02-17 17:17:40 +0700

    Move header to App component as a common components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,16 @@
+import clsx from 'clsx';
+
+import styles from '@styles/App.module.css';
 import { WelcomePage } from '@pages/index';
+import { Header } from './common';
 
 function App() {
-  return <WelcomePage />;
+  return (
+    <div className={clsx(styles.app)}>
+      <Header isHidden />
+      <WelcomePage />
+    </div>
+  );
 }
 
 export default App;
