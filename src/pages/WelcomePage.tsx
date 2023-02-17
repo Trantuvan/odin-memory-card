acd7d7fc5ab746ccff906ad7bc71790607d9b02e
trantuvan <trantuvan.kan@gmail.com>
2023-02-17 17:18:58 +0700

    add begining words & guide to play

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,12 +1,41 @@
+/* eslint-disable react/no-unescaped-entities */
 import clsx from 'clsx';
 
 import styles from '@styles/WelcomePage.module.css';
-import { Header } from '@components/common';
+import { useState } from 'react';
 
 function WelcomePage() {
+  const [showRule, setRule] = useState(false);
+
+  const guideContent =
+    showRule === false ? (
+      <>
+        Forge a garden where no two blooms are ever the same.
+        <br /> <span>∾</span> <br />
+        A bounty of unique and mysterious plant seedlings await your cultivation.
+        <br /> <span>∾</span> <br />
+        Successfully grow 20 of them and ascend to the throne of the Floral Master!
+      </>
+    ) : (
+      <>
+        Choose a card, but don't click on the same one twice.
+        <br /> <span>∾</span> <br />
+        Each correct choice gives you 1 point.
+        <br /> <span>∾</span> <br />
+        Score 20 points and win the game!
+      </>
+    );
   return (
     <div className={clsx(styles.welcomeSection)}>
-      <Header isHidden />;
+      <p className={clsx(styles.guideBox)}>{guideContent}</p>
+      <div className={clsx(styles.actions)}>
+        <button type="button" className={clsx(styles.btn, styles.btnLeft)} onClick={() => setRule(!showRule)}>
+          {showRule ? 'Fantasy Realm' : 'Show Rule'}
+        </button>
+        <button type="button" className={clsx(styles.btn, styles.btnRight)}>
+          Play
+        </button>
+      </div>
     </div>
   );
 }
