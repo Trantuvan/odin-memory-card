febbb19c0a9e54d29d531694bf6ba28aa6f731b6
trantuvan <trantuvan.kan@gmail.com>
2023-02-19 10:25:48 +0700

    manually import imgs into array to render list cards

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,31 +1,14 @@
 import clsx from 'clsx';
-import { useLayoutEffect, useState } from 'react';
 
 import Card from '@components/Card';
-import { importAllImgs } from '@helpers/index';
 import styles from '@styles/GamePage.module.css';
+import getImgs from '@helpers/getImgs';
 
 function GamePage() {
-  const [urlArr, setUrlArr] = useState<
-    {
-      id: string;
-      urlObj: URL;
-    }[]
-  >();
-
-  useLayoutEffect(() => {
-    async function getImgs() {
-      const data = await importAllImgs();
-      console.log(data);
-      setUrlArr(data);
-    }
-    getImgs();
-  }, []);
-
   return (
     <div className={clsx(styles.cardContainer)}>
-      {urlArr?.map((url) => (
-        <Card key={url.id} imgUrl={url.urlObj.href} />
+      {getImgs.map((img, index) => (
+        <Card key={index} imgUrl={img} />
       ))}
     </div>
   );
