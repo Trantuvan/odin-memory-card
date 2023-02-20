b7f89b3142ec54754577bf0f484c157d65786030
trantuvan <trantuvan.kan@gmail.com>
2023-02-20 23:05:09 +0700

    working in passing annoucement to modal

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,31 +1,54 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import clsx from 'clsx';
 
 import styles from '@styles/App.module.css';
 import { GamePage, WelcomePage } from '@pages/index';
-import { Header } from './common';
+import { Header, Modal } from './common';
 
 function App() {
   const [showGamePage, setShow] = useState(true);
+  const [isWon, setIsWon] = useState(false);
   const [currScore, setCurrScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
   const [selectedCards, setSelectedCards] = useState<number[]>([]);
 
-  const handleClick = (e: React.MouseEvent, index: number) => {
+  const modalRef = useRef<HTMLDialogElement>(null);
+
+  const handleClick = (e: React.MouseEvent, index: number): undefined => {
     const indexSelected = selectedCards.find((cardIndex) => cardIndex === index);
     console.log({
       isSelectedCard: indexSelected,
       index,
     });
 
+    // *lose Case
     if (indexSelected) {
       console.log('card selected');
       setBestScore(currScore);
-    } else {
-      console.log('add new card');
-      setSelectedCards((prevCards) => [...prevCards, index]);
-      setCurrScore((prevScore) => prevScore + 1);
+      modalRef.current?.showModal();
+
+      // *reset selectedCards & currentScore
+      setSelectedCards([]);
+      setCurrScore(0);
+      return undefined;
     }
+
+    if (currScore === 20) {
+      console.log(`currScore: ${currScore}`);
+      setIsWon(true);
+      modalRef.current?.showModal();
+
+      // *reset selectedCards & currentScore
+      setSelectedCards([]);
+      setCurrScore(0);
+      return undefined;
+    }
+
+    console.log('add new card');
+    setSelectedCards((prevCards) => [...prevCards, index]);
+    setCurrScore((prevScore) => prevScore + 1);
+
+    return undefined;
   };
 
   return (
@@ -36,6 +59,7 @@ function App() {
       ) : (
         <WelcomePage handleTogglePage={() => setShow(!showGamePage)} />
       )}
+      <Modal ref={modalRef} isWon={isWon} handleIsWon={setIsWon} handleCloseModal={() => modalRef.current?.close()} />
     </div>
   );
 }
