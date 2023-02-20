4dbf57365f5a20b244c3d0b19df1df94840217f7
trantuvan <trantuvan.kan@gmail.com>
2023-02-20 19:42:50 +0700

    passing 2 props types number to reflect score from App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,10 +7,12 @@ import { Header } from './common';
 
 function App() {
   const [showGamePage, setShow] = useState(false);
+  const [currScore, setCurrScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
 
   return (
     <div className={clsx(styles.app)}>
-      <Header isHidden={!showGamePage} />
+      <Header currScore={currScore} bestScore={bestScore} isHidden={!showGamePage} />
       {showGamePage ? <GamePage /> : <WelcomePage handleTogglePage={() => setShow(!showGamePage)} />}
     </div>
   );
